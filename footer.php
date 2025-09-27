7aee9d67b795d1af70eac9f5f1db2d52f81f2a43
unknown <oscar.ronaldo.96@gmail.com>
2025-09-26 22:25:06 -0500

    arreglo de conexiones

--- footer.php
+++ footer.php
@@ -13,3 +13,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Script para formatear inputs numéricos -->
+<?php
+if (isset($conexion) && $conexion instanceof mysqli) {
+    $conexion->close();
+}
+?>
